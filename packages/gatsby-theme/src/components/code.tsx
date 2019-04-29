62ef9396e365ff178e94904c485f5b678a7bc109
Rohit <rohit@novvuminc.com>
2019-04-29 16:50:08 -0700

    added copy button

--- packages/gatsby-theme/src/components/code.tsx
+++ packages/gatsby-theme/src/components/code.tsx
@@ -2,7 +2,21 @@ import React from 'react'
 import Highlight, { defaultProps } from 'prism-react-renderer'
 import { LiveProvider, LiveEditor, LiveError, LivePreview } from 'react-live'
 
-export const Code = ({ codeString, language, ...props }) => {
+export const Code = (props) => {
+  return <div style={{ position: "relative" }}>
+    <CodeRenderer {...props} />
+    <button
+      style={{ position: "absolute", top: 10, right: 10 }}
+      onClick={() => {
+        // Copying code here!
+      }}
+    >
+      Copy
+    </button>
+  </div>
+}
+
+const CodeRenderer = ({ codeString, language, ...props }) => {
   console.log(codeString, language);
   if (props['react-live']) {
     return (
