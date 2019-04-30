1d40fb6772d37ace4802b5d3eef647f3532c4b2c
Rohit <rohit@novvuminc.com>
2019-04-30 14:07:20 -0700

    made components exportable

---
+++ packages/gatsby-theme/index.ts
@@ -0,0 +1,6 @@
+// no-op
+import Button from './src/components/Button'
+
+export {
+    Button
+}
---
+++ packages/gatsby-theme/src/components/Button.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Button() {
+    return (
+        <button>
+            This is a button component
+        </button>
+    )
+}
