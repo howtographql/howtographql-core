852c8f373c6bb302226466e179eebb5dd9033e3e
Kelsey Yim <kelseyann.yim@gmail.com>
2019-04-30 17:12:33 -0700

    Added rebass styles

--- packages/gatsby-theme/src/components/layout.tsx
+++ packages/gatsby-theme/src/components/layout.tsx
@@ -1,9 +1,9 @@
-import { RouterProps } from '@reach/router';
-import * as React from 'react';
-import Helmet from 'react-helmet';
-import { theme, ThemeProvider, styled } from '../styles';
-import { useLayoutQuery } from '../hooks/useLayoutQuery';
-import Header from './shared/Header';
+import { RouterProps } from "@reach/router";
+import * as React from "react";
+import Helmet from "react-helmet";
+import { theme, ThemeProvider, styled } from "../styles";
+import { useLayoutQuery } from "../hooks/useLayoutQuery";
+import Header from "./shared/Header";
 
 const MainLayout = styled.main`
   max-width: 90%;
@@ -19,7 +19,7 @@ type LayoutProps = React.ReactNode & RouterProps;
 const Layout: React.FunctionComponent<LayoutProps> = ({ children }) => {
   const { site } = useLayoutQuery();
 
-  const { title, description, keywords } = site.siteMetadata;
+  const { title, description, keywords } = site!.siteMetadata;
 
   return (
     <ThemeProvider theme={theme}>
@@ -27,8 +27,8 @@ const Layout: React.FunctionComponent<LayoutProps> = ({ children }) => {
         <Helmet
           title={title}
           meta={[
-            { name: 'description', content: description },
-            { name: 'keywords', content: keywords || undefined },
+            { name: "description", content: description },
+            { name: "keywords", content: keywords || undefined }
           ]}
         >
           <html lang="en" />
