9689e97298f3381e0342b574530a29ac584af1a7
Kelsey Yim <kelseyann.yim@gmail.com>
2019-04-30 17:49:10 -0700

    Added functional links to Header tabs

--- packages/gatsby-theme/src/components/shared/Header.tsx
+++ packages/gatsby-theme/src/components/shared/Header.tsx
@@ -49,9 +49,9 @@ const Nav = withTheme(
             <Logo maxHeight={theme.sizes.brandHeight} />
           </Container>
           <Container justifyContent="center">
-            <NavLink to="/tutorials">Fundamentals</NavLink>
-            <NavLink to="/tutorials">Guides</NavLink>
-            <NavLink to="/tutorials">Community</NavLink>
+            <NavLink to="/fundamentals">Fundamentals</NavLink>
+            <NavLink to="/guides">Guides</NavLink>
+            <NavLink to="/community">Community</NavLink>
             <NavLink to="/components">Components</NavLink>
           </Container>
           <Container justifyContent="flex-end">other stuff</Container>
