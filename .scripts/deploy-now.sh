c129e5c1d31127d05e379745200d9299b1380e5f
Rohit <rohit@novvuminc.com>
2019-05-07 14:48:37 -0700

    more refinements

--- .scripts/deploy-now.sh
+++ .scripts/deploy-now.sh
@@ -5,13 +5,13 @@ set -ex
 # ENV Variables, Note: ACCESS_TOKEN and NOW_TOKEN in gitlab-ci
 ORIGIN='*'
 # NOW config
-TEAM=$NOW_TEAM
-PROJECT=$NOW_PROJECT
+TEAM=howtographql
+PROJECT=api
 ALIAS=$NOW_ALIAS
 
 export PATH="./node_modules/.bin:$PATH"
 # 1. Wait for deployment ready
-now --target production -A now.$STAGE.json -e STAGE=$STAGE -e ENGINE_TAG=${CIRCLE_BRANCH:-staging} --token "$NOW_TOKEN" --scope $TEAM
+now --target $STAGE -e STAGE=$STAGE --token "$NOW_TOKEN"
 # URL=$(now --target $STAGE -e STAGE=$STAGE -e ENGINE_TAG=${CIRCLE_BRANCH:-staging} --token "$NOW_TOKEN" --scope $TEAM)
 # 2. Alias
 # now alias set "$URL" "$ALIAS" --token "$NOW_TOKEN" --scope $TEAM
