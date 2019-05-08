17984e21ce604a488830c704d133294e1b29ee5e
Alli Colyer <allicolyer@gmail.com>
2019-05-08 12:14:22 -0700

    refactor componenets and add types

--- packages/gatsby-theme/src/components/templates/TutorialOverview.tsx
+++ packages/gatsby-theme/src/components/templates/TutorialOverview.tsx
@@ -1,13 +1,13 @@
-import React from "react";
-import Layout from "../layout";
-import Chapter from "../chapter";
-import { TutorialOverviewQuery } from "src/graphqlTypes";
-import { Author } from "../Author";
-import TutorialHeader from "../overview/TutorialHeader";
-import { Heading, Flex, Box } from "../shared/base";
-import ProgressBar from "../overview/ProgressBar";
-import IconButton from "../Button";
-import { Content } from "../shared/styledHelpers";
+import React from 'react';
+import Layout from '../layout';
+import Chapter from '../chapter';
+import { TutorialOverviewQuery } from 'src/graphqlTypes';
+import AuthorList  from '../Author';
+import TutorialHeader from '../overview/TutorialHeader';
+import { Heading, Flex, Box } from '../shared/base';
+import ProgressBar from '../overview/ProgressBar';
+import CustomButton from '../Button';
+import { Content } from '../shared/styledHelpers';
 
 interface PageTemplateProps {
   data: TutorialOverviewQuery;
@@ -23,29 +23,19 @@ const PageTemplate: React.FunctionComponent<PageTemplateProps> = ({ data }) => {
               width={1 / 2}
               title={data!.overview!.frontmatter!.tutorialTitle}
               description={data!.overview!.frontmatter!.description}
-              tags={["React", "Apollo", "Javascript"]}
+              tags={['React', 'Apollo', 'Javascript']}
             />
           </Box>
           <Box width={1 / 4} m={3}>
-            <IconButton type="tutorial" link="http://localhost:8000/components">
-              Continue Tutorial
-            </IconButton>
+            <CustomButton type="tutorial">Continue Tutorial</CustomButton>
             <Box m={3}>
               <ProgressBar percentage={33} width={100} />
             </Box>
             <Flex>
-              <IconButton type="github" link="http://localhost:8000/components">
-                Github
-              </IconButton>
-              <IconButton
-                type="spectrum"
-                link="http://localhost:8000/components"
-              >
-                Spectrum
-              </IconButton>
+              <CustomButton type="github">Github</CustomButton>
+              <CustomButton type="spectrum">Spectrum</CustomButton>
             </Flex>
-
-            <Author authors={authors} />
+            <AuthorList authors={authors} />
           </Box>
         </Flex>
         <div>
@@ -104,15 +94,15 @@ export const query = graphql`
 
 let authors = [
   {
-    name: "Kelsey Yim",
-    job: "Software Developer",
-    picture: "https://www.w3schools.com/howto/img_avatar2.png"
+    name: 'Kelsey Yim',
+    job: 'Software Developer',
+    picture: 'https://www.w3schools.com/howto/img_avatar2.png',
   },
   {
-    name: "Alli Colyer",
-    job: "Party Planner",
-    picture: "https://www.w3schools.com/howto/img_avatar2.png"
-  }
+    name: 'Alli Colyer',
+    job: 'Party Planner',
+    picture: 'https://www.w3schools.com/howto/img_avatar2.png',
+  },
 ];
 
 export default PageTemplate;
