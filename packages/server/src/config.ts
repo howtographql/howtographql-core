74c9c223478dc5fc05783066f621fb2d5fdec63b
Alli Colyer <allicolyer@gmail.com>
2019-05-10 12:51:53 -0700

    change from Github App to OAuth App

--- packages/server/src/config.ts
+++ packages/server/src/config.ts
@@ -1,21 +1,23 @@
 export type Config = {
-    github: {
-        CLIENT_ID: string;
-        CLIENT_SECRET: string;
-    },
-    jwt: {
-        SECRET: string
-    }
-};
-// https://github.com/login/oauth/authorize?client_id=Iv1.5345771c55b8eb37&redirect_uri=http://localhost:8000
+  github: {
+    CLIENT_ID: string
+    CLIENT_SECRET: string
+  }
+  jwt: {
+    SECRET: string
+  }
+}
+// https://github.com/login/oauth/authorize?client_id=2f9502dd9f0f44ca2e3b&redirect_uri=http://localhost:8000
 const config: Config = {
-    github: {
-        CLIENT_ID: process.env.GQL_GITHUB_CLIENT_ID || "Iv1.5345771c55b8eb37",
-        CLIENT_SECRET: process.env.GQL_GITHUB_CLIENT_SECRET || "19cef65a19b44c2c28e86f3969d83583cf071b02"
-    },
-    jwt: {
-        SECRET: process.env.GQL_JWT_SECRET || "helloworld"
-    }
+  github: {
+    CLIENT_ID: process.env.GQL_GITHUB_CLIENT_ID || '2f9502dd9f0f44ca2e3b',
+    CLIENT_SECRET:
+      process.env.GQL_GITHUB_CLIENT_SECRET ||
+      'c89179109eec632c40ae3b589ed231497eb78cf1',
+  },
+  jwt: {
+    SECRET: process.env.GQL_JWT_SECRET || 'helloworld',
+  },
 }
 
-export default config;
+export default config
