7d1f8beb97d29779a2416528bf52c41b830768ee
Rohit <rohit@novvuminc.com>
2019-05-23 10:40:18 -0700

    logging tutorialTitle

--- packages/gatsby-theme/src/components/templates/Tutorial.tsx
+++ packages/gatsby-theme/src/components/templates/Tutorial.tsx
@@ -18,7 +18,7 @@ const TutorialLayout: React.FunctionComponent<TutorialLayoutProps> = ({
   if (!data) {
     return null;
   }
-  console.log(data);
+  console.log(JSON.stringify(data.tutorialTitle, null, 2));
   const { pageTitle } = data!.mdx!.frontmatter!;
   const tutorialTitle = optionalChaining(() => data!.tutorialTitle!.frontmatter!.tutorialTitle!);
   const chapters = optionalChaining(() => data!.pageTitles!.edges!.map(
