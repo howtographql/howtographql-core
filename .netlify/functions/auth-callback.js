5052c1cb86d075c182656292de770f146f1a8568
Rohit <rohit@novvuminc.com>
2019-05-24 12:40:19 -0700

    added auth directory

--- .netlify/functions/auth-callback.js
+++ .netlify/functions/auth-callback.js
@@ -5,24 +5,24 @@ exports.handler = function(event, context, callback) {
       "Content-Type": "text/html"
     },
     body: `
-              <html>
-                  <body>
-                      <script>
-                        function getParameterByName(name, url_string) {
-                          var url = new URL(url_string);
-                          var c = url.searchParams.get("code");    
-                          return c;
-                        }
-                        
-                        (function Callback() {
-                          let githubCode = getParameterByName("code", window.location.href);
-                          console.log(githubCode)
-                          let targetWindow = window.opener;
-                          targetWindow.postMessage(githubCode, "*");
-                        })()
-                      </script>
-                  </body>
-              </html>
-          `
+      <html>
+          <body>
+              <script>
+                function getParameterByName(name, url_string) {
+                  var url = new URL(url_string);
+                  var c = url.searchParams.get("code");    
+                  return c;
+                }
+                
+                (function Callback() {
+                  let githubCode = getParameterByName("code", window.location.href);
+                  console.log(githubCode)
+                  let targetWindow = window.opener;
+                  targetWindow.postMessage(githubCode, "*");
+                })()
+              </script>
+          </body>
+      </html>
+    `
   });
 };
