6f0d79a4785c351ce2eb660dd30a623eb771d157
Alli Colyer <allicolyer@gmail.com>
2019-06-11 10:34:57 -0700

    add Apollo Fetch and graphql-cool endpoint

--- packages/gatsby-theme/gatsby-node.js
+++ packages/gatsby-theme/gatsby-node.js
@@ -4,8 +4,25 @@ const {
   getTutorialOverviewSlug,
 } = require('./src/utils/getTutorialSlug.js');
 
-const { Mutation } = require('react-apollo');
-const { gql } = require('graphql-tag');
+const { createApolloFetch } = require('apollo-fetch');
+
+const createPrismaNodes = async variables => {
+  const uri = 'https://api.graph.cool/simple/v1/HowtoGraphQL2';
+  const apolloFetch = createApolloFetch({ uri });
+  const query = `
+  mutation createTutorial($gatsbyID: String!, $name: String!, $numberofChapters: Int!) {
+    createTutorial(gatsbyID: $gatsbyID, name: $name, numberofChapters: $numberofChapters, numberOfStudents: 9, upvotes:10){
+      id
+    }
+  }`;
+  return apolloFetch({ query, variables })
+    .then(response => {
+      console.log(response);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+};
 
 exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions;
@@ -32,32 +49,32 @@ exports.createPages = async ({ graphql, actions }) => {
     }
   `).catch(error => console.error(error));
 
-  const UPSERT_TUTORIAL = gql`
-    mutation UpsertTutorial(
-      $gatsbyID: String!
-      $name: String!
-      $numberofChapters: Int!
-    ) {
-      upsertTutorial(
-        gatsbyId: $gatsbyID
-        name: $name
-        numberofChapters: $numberofChapters
-      ) {
-        gatsbyId
-        name
-        numberofChapters
-      }
-    }
-  `;
-
   data.allMdx.edges.forEach(({ node }) => {
     const tutorialPath = getTutorialSlug(node.fileAbsolutePath);
     const overviewPageSlug = getTutorialOverviewSlug(node.fileAbsolutePath);
     const overviewTemplate = require.resolve(
       './src/components/templates/TutorialOverview.tsx',
     );
+
     //TODO: find a better way to ID posts & overviews Also a better way to query for them
     if (node.frontmatter.tutorialTitle) {
+      //counts the number of chapters in each tutorial by counting how many files
+      //there are that contain the overviewpage slug. Subtract one for the overview page.
+      console.log(data.allMdx.edges.length);
+
+      const numberofChapters = data.allMdx.edges.filter(chapterNode => {
+        // console.log(chapterNode.node.fileAbsolutePath);
+        // console.log(overviewPageSlug);
+        return chapterNode.node.fileAbsolutePath.includes(overviewPageSlug);
+      });
+
+      const variables = {
+        gatsbyID: node.frontmatter.id,
+        name: node.frontmatter.tutorialTitle,
+        numberofChapters: numberofChapters.length - 1,
+      };
+
+      createPrismaNodes(variables);
       return createPage({
         path: overviewPageSlug,
         component: overviewTemplate,
